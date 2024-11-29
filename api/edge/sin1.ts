9a42fb3a997026bdfe5c41bec0fff7651479117b
Maximilian Kaske <maximilian@kaske.org>
2024-11-29 15:35:37 +0100

    chore: fix region authentification

--- api/edge/sin1.ts
+++ api/edge/sin1.ts
@@ -9,6 +9,14 @@ export const config = {
 
 export default async function handler(req: Request): Promise<Response> {
   const region = process.env.VERCEL_REGION || "unknown";
+  const authHeader = req.headers.get("authorization");
+
+  if (
+    !process.env.PING_SECRET ||
+    authHeader !== `Bearer ${process.env.PING_SECRET}`
+  ) {
+    return new Response("Unauthorized", { status: 401 });
+  }
 
   if (req.method !== "POST") {
     return new Response(`Method Not Allowed ('${region}')`, { status: 405 });
